c838bd270ba3e66f6f47beabc54e8dd629bf9610
yuvalzohar36 <56931187+yuvalzohar36@users.noreply.github.com>
2020-01-03 11:16:24 +0200

    Update Mp3_Player.py

--- Mp3_Player.py
+++ Mp3_Player.py
@@ -1,5 +1,4 @@
-####### yuval___zohar
-####### dont forget to make songs library (.mp3)
+
 # -*- coding: utf-8 -*-
 
 # Form implementation generated from reading ui file 'project.ui'
@@ -20,6 +19,7 @@ class Ui_MainWindow(object):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(610, 593)
         self.listofsongs=[]
+        self.volume=0.3
         self.index=0
         self.clicktimes=0
         self.centralwidget = QtWidgets.QWidget(MainWindow)
@@ -33,7 +33,7 @@ class Ui_MainWindow(object):
         self.Title.setFont(font)
         self.Title.setObjectName("Title")
         self.msg = QtWidgets.QLabel(self.centralwidget)
-        self.msg.setGeometry(QtCore.QRect(310, 380, 291, 61))
+        self.msg.setGeometry(QtCore.QRect(310, 420, 291, 61))
         font = QtGui.QFont()
         font.setPointSize(12)
         font.setBold(True)
@@ -60,6 +60,70 @@ class Ui_MainWindow(object):
         self.SongList = QtWidgets.QListWidget(self.centralwidget)
         self.SongList.setGeometry(QtCore.QRect(10, 90, 271, 281))
         self.SongList.setObjectName("SongList")
+
+
+
+
+
+
+
+
+
+        self.PlusVolumeButton = QtWidgets.QPushButton(self.centralwidget)
+        self.PlusVolumeButton.setGeometry(QtCore.QRect(530, 320, 31, 31))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.PlusVolumeButton.setFont(font)
+        self.PlusVolumeButton.setObjectName("PlusVolumeButton")
+        self.PlusVolumeButton.clicked.connect(self.plusfunc)
+        self.MinusVolumeButton = QtWidgets.QPushButton(self.centralwidget)
+        self.MinusVolumeButton.setGeometry(QtCore.QRect(530, 400, 31, 31))
+        self.MinusVolumeButton.clicked.connect(self.minusfunc)
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.MinusVolumeButton.setFont(font)
+        self.MinusVolumeButton.setObjectName("MinusVolumeButton")
+        self.CurrentVolume = QtWidgets.QLabel(self.centralwidget)
+        self.CurrentVolume.setGeometry(QtCore.QRect(535, 360, 41, 41))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        self.CurrentVolume.setFont(font)
+        self.CurrentVolume.setObjectName("CurrentVolume")
+        
+
+
+
+
+        self.ForwardButton = QtWidgets.QPushButton(self.centralwidget)
+        self.ForwardButton.setGeometry(QtCore.QRect(440, 200, 100, 30))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.ForwardButton.setFont(font)
+        self.ForwardButton.setObjectName("ForwardButton")
+        self.ForwardButton.clicked.connect(self.moveforward)
+        self.BackwardButton = QtWidgets.QPushButton(self.centralwidget)
+        self.BackwardButton.setGeometry(QtCore.QRect(320, 200, 100, 30))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.BackwardButton.setFont(font)
+        self.BackwardButton.setObjectName("BackwardButton")
+        self.BackwardButton.clicked.connect(self.movebackward)
+
+
+
+
+
+
+
+
+
+        
     
 
         MainWindow.setCentralWidget(self.centralwidget)
@@ -82,6 +146,68 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+    def plusfunc(self):
+        if len(self.listofsongs)==0:
+            self.msg.show()
+        if len(self.listofsongs)>0 :
+            self.msg.hide()
+            pygame.mixer.music.set_volume(pygame.mixer.music.get_volume()+0.04)
+            print(str(pygame.mixer.music.get_volume()))
+            self.CurrentVolume.setText((str(int(((pygame.mixer.music.get_volume()*100))))))
+            #print(pygame.mixer.music.get_pos())
+            #if pygame.mixer.music.get_busy()==True:
+            #    pygame.mixer.music.set_pos((pygame.mixer.music.get_pos()+2000)/1000)
+            '''elif len(self.listofsongs)>self.index+1:
+                self.index+=1
+                pygame.mixer.music.load(self.listofsongs[self.index])
+                pygame.mixer.music.play()
+                self.playinglabel.setText("Playing Now \n" +self.listofsongs[self.index])'''
+    def moveforward(self):
+        if len(self.listofsongs)>0 :
+                self.msg.hide()
+                if pygame.mixer.music.get_busy()==True:
+                    pygame.mixer.music.set_pos((pygame.mixer.music.get_pos()+2000)/1000)
+                elif len(self.listofsongs)>self.index+1:
+                    self.index+=1
+                    pygame.mixer.music.load(self.listofsongs[self.index])
+                    pygame.mixer.music.play()
+                    self.playinglabel.setText("Playing Now \n" +self.listofsongs[self.index])
+                    self.SongList.item(self.index).setSelected(True)
+                elif len(self.listofsongs)==self.index+1:
+                    self.index=0
+                    pygame.mixer.music.load(self.listofsongs[self.index])
+                    pygame.mixer.music.play()
+                    self.playinglabel.setText("Playing Now \n" +self.listofsongs[self.index])
+                    self.SongList.item(self.index).setSelected(True)
+
+
+    def movebackward(self):
+         if len(self.listofsongs)>0 :
+                self.msg.hide()
+                if pygame.mixer.music.get_busy()==True:
+                    pygame.mixer.music.set_pos((pygame.mixer.music.get_pos()-2000)/1000)
+                elif self.index>=1:
+                    self.index-=1
+                    pygame.mixer.music.load(self.listofsongs[self.index])
+                    pygame.mixer.music.play()
+                    self.playinglabel.setText("Playing Now \n" +self.listofsongs[self.index])
+                    self.SongList.item(self.index).setSelected(True)
+
+
+
+
+
+    def minusfunc(self):
+        if len(self.listofsongs)==0:
+            self.msg.show()
+        if len(self.listofsongs)>0 :
+            self.msg.hide()
+            pygame.mixer.music.set_volume(pygame.mixer.music.get_volume()-0.04)
+            print(str(pygame.mixer.music.get_volume()))
+            self.CurrentVolume.setText((str(int(((pygame.mixer.music.get_volume()*100))))))
+
+
+        
     def play(self):
             if len(self.listofsongs)==0:
                 self.msg.show()
@@ -102,6 +228,7 @@ class Ui_MainWindow(object):
                 self.index+=1
                 pygame.mixer.init()
                 pygame.mixer.music.load(self.listofsongs[self.index])
+                self.SongList.item(self.index).setSelected(True)
                 pygame.mixer.music.play()
                 self.playinglabel.setText("Playing Now \n" +self.listofsongs[self.index])
           
@@ -113,6 +240,7 @@ class Ui_MainWindow(object):
                 self.index-=1
                 pygame.mixer.init()
                 pygame.mixer.music.load(self.listofsongs[self.index])
+                self.SongList.item(self.index).setSelected(True)
                 pygame.mixer.music.play()
                 self.playinglabel.setText("Playing Now \n" +self.listofsongs[self.index])
 
@@ -130,8 +258,13 @@ class Ui_MainWindow(object):
         self.msg.hide()
         pygame.mixer.init()
         pygame.mixer.music.load(self.listofsongs[self.index])
+        self.SongList.item(self.index).setSelected(True)
+        #print(os.path.getsize(self.listofsongs[self.index]))
         pygame.mixer.music.play()
+        pygame.mixer.music.set_volume(self.volume)
         self.playinglabel.setText("Playing Now \n" +self.listofsongs[self.index])
+        print(str(pygame.mixer.music.get_volume()))
+        self.CurrentVolume.setText((str(int(((pygame.mixer.music.get_volume()*100))))))
 
 
 
@@ -142,7 +275,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Mp3 Player ---- @yuvalzohar"))
         self.Title.setText(_translate("MainWindow", "Mp3 Player"))
         self.PreviousButton.setText(_translate("MainWindow", "Previous Song"))
         self.NextButton.setText(_translate("MainWindow", "Next Song"))
@@ -150,6 +283,11 @@ class Ui_MainWindow(object):
         self.ChooseButton.setText(_translate("MainWindow", "Choose Directory"))
         #self.playinglabel.setText(_translate("MainWindow", "Playing now : \n " + " -------"))
         self.msg.setText(_translate("MainWindow", "You must choose directory first !"))
+        self.PlusVolumeButton.setText(_translate("MainWindow", "+ "))
+        self.MinusVolumeButton.setText(_translate("MainWindow", "-"))
+        self.ForwardButton.setText(_translate("MainWindow", "Forward"))
+        self.BackwardButton.setText(_translate("MainWindow", "Backward"))
+        self.CurrentVolume.setText(_translate("MainWindow", "40"))
         __sortingEnabled = self.SongList.isSortingEnabled()
         self.SongList.setSortingEnabled(False)
         self.SongList.setSortingEnabled(__sortingEnabled)
